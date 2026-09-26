c0a855dc17e43418381f3aee804de6087ab37886
weverton790458597 <ma.imoveis.slz@gmail.com>
2026-09-26 12:22:15 -0300

    Add files via upload

---
+++ 7904/sw.js
@@ -0,0 +1,16 @@
+const CACHE = "autoflow-shell-v1";
+const ASSETS = ["./","./index.html","./styles.css","./app-ui.js","./manifest.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
